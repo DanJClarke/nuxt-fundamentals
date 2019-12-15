67f568d75fdfe7930a2dcae27908174580d6900e
Daniel Clarke <daniel.clarke@Daniels-Mac-mini.local>
2019-12-15 12:46:59 +0000

    Create a Navbar component and link it to the default component'

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
+    <TheNavbar/>
     <nuxt />
   </div>
 </template>
+
+<script>
+  import TheNavbar from '@/components/TheNavbar'
+  export default {
+    components: {
+      TheNavbar
+    }
+  }
+</script>
 
 <style>
 .button--green {
